4d67de95c942a299093f9ffa1d1a88c758740284
PaulBailey-1 <paul.ryan.bailey@gmail.com>
2025-04-17 12:50:34 -0500

    Started implementing memory module, added ps2 module

--- src/cpu.v
+++ src/cpu.v
@@ -15,20 +15,21 @@ module main();
 
     counter ctr(halt,clk);
 
-    // PC, also f0
+    // PC
     reg [15:0]pc = 16'h0000;
 
     // memory
     wire [15:0]mem_inst;
-    wire [15:1]mem_addr;
+    wire mem_read_en;
+    wire [15:0]mem_addr;
     wire [15:0]mem_data;
     wire mem_write_en;
-    wire [15:1]mem_write_addr;
+    wire [15:0]mem_write_addr;
     wire [15:0]mem_write_data;
 
     mem mem(clk, 
-        pc[15:1], mem_inst,
-        mem_addr, mem_data,
+        pc[15:0], mem_inst,
+        mem_read_en, mem_addr, mem_data,
         mem_write_en, mem_write_addr, mem_write_data
     );
     
--- src/mem.v
+++ src/mem.v
@@ -1,33 +1,56 @@
 `timescale 1ps/1ps
 
 module mem(input clk,
-    input [15:1]raddr0_, output [15:0]rdata0_,
-    input [15:1]raddr1_, output [15:0]rdata1_,
-    input wen, input [15:1]waddr, input [15:0]wdata);
+    input [15:0]raddr0, output [15:0]rdata0,
+    input ren, input [15:0]raddr1, output reg [15:0]rdata1,
+    input wen, input [15:0]waddr, input [15:0]wdata);
 
-    reg [15:0]data[0:16'h7fff];
+    localparam TILEMAP_START = 16'hc000;
+    localparam FRAMEBUFFER_START = 16'he000;
+    localparam IO_START = 16'hf000;
+    localparam PS2_REG = 16'hf000;
+
+    reg [15:0]ram[0:16'hbfff]; // 768Kb (0x0000-0xBFFF)
+    reg [15:0]tile_map[0:16'h2000]; // 128Kb (0xC000-0xDFFF)
+    reg [15:0]frame_buffer[0:16'h1000]; // 64Kb (0xE000-0xEFFF)
+
+    wire ps2_clk;
+    wire ps2_data;
+    wire ps2_ren = raddr1 == PS2_REG & ren;
+    wire [15:0]ps2_data_out;
+    ps2 ps2(ps2_clk, ps2_data, clk, ps2_ren, ps2_data_out);
 
     /* Simulation -- read initial content from file */
     initial begin
-        $readmemh("mem.hex",data);
+        $readmemh("program.hex", ram);
+        $readmemb("tilemap.bin", tile_map);
     end
 
-    reg [15:1]raddr0;
-    reg [15:0]rdata0;
-
-    reg [15:1]raddr1;
-    reg [15:0]rdata1;
-
-    assign rdata0_ = rdata0;
-    assign rdata1_ = rdata1;
+    assign rdata0 = raddr0 < TILEMAP_START ? ram[raddr0] :
+                    raddr0 < FRAMEBUFFER_START ? tile_map[raddr0 - TILEMAP_START] :
+                    raddr0 < IO_START ? tile_map[raddr0 - FRAMEBUFFER_START] : 0;
 
     always @(posedge clk) begin
-        raddr0 <= raddr0_;
-        raddr1 <= raddr1_;
-        rdata0 <= data[raddr0];
-        rdata1 <= data[raddr1];
+
+        if (ren) begin
+            if (raddr1 < TILEMAP_START) begin
+                rdata1 <= ram[raddr1];
+            end else if (raddr1 < FRAMEBUFFER_START) begin
+                rdata1 <= tile_map[raddr1 - TILEMAP_START];
+            end else if (raddr1 < IO_START) begin
+                rdata1 <= frame_buffer[raddr1 - FRAMEBUFFER_START];
+            end else if (raddr1 == PS2_REG) begin
+                rdata1 <= ps2_data_out;
+            end
+        end
         if (wen) begin
-            data[waddr] <= wdata;
+            if (waddr < TILEMAP_START) begin
+                ram[waddr] <= wdata;
+            end else if (waddr < FRAMEBUFFER_START) begin
+                tile_map[waddr - TILEMAP_START] <= wdata;
+            end else if (waddr < IO_START) begin
+                frame_buffer[waddr - FRAMEBUFFER_START] <= wdata;
+            end
         end
     end
 
