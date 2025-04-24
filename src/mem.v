7f7cb42c61cc0ecd052a874dcab96030cbcd0964
Brooks Bryant <brooks.bryant@icloud.com>
2025-04-24 00:09:34 -0500

    scale reg

--- src/mem.v
+++ src/mem.v
@@ -12,6 +12,9 @@ module mem(input clk,
     localparam FRAMEBUFFER_START = 16'he000;
     localparam IO_START = 16'hf000;
     localparam PS2_REG = 16'hffff;
+    localparam VSCROLL_REG = 16'hfffe;
+    localparam HSCROLL_REG = 16'hfffd;
+    localparam SCALE_REG = 16'hfffc;
 
     (* ram_style = "block" *) reg [15:0]ram[0:16'hbfff]; // 768Kb (0x0000-0xBFFF)
     (* ram_style = "block" *) reg [15:0]tile_map[0:16'h1fff]; // 128Kb (0xC000-0xDFFF)
@@ -63,6 +66,10 @@ module mem(input clk,
     reg [9:0]display_pixel_x;
     reg [9:0]display_pixel_y;
     reg [15:0]display_tilemap_out;
+
+    reg [15:0]scale_reg;
+    reg [15:0]vscroll_reg;
+    reg [15:0]hscroll_reg;
     
     // Display pixel retrevial
     wire [15:0] display_frame_addr = ({9'b0, pixel_x[9:3]} + {2'b0, pixel_y[9:3], 7'b0}); // (x / 8 + y /8 * 128)
@@ -88,8 +95,8 @@ module mem(input clk,
 
         display_framebuffer_out <= frame_buffer[display_frame_addr[15:1]];
         display_odd_tile <= display_frame_addr[0];
-        display_pixel_x <= pixel_x;
-        display_pixel_y <= pixel_y;
+        display_pixel_x <= (pixel_x >> scale_reg);
+        display_pixel_y <= (pixel_y >> scale_reg);
         display_tilemap_out <= tile_map[pixel_idx];
 
         if (wen) begin
@@ -99,6 +106,12 @@ module mem(input clk,
                 tile_map[waddr - TILEMAP_START] <= wdata;
             end else if (waddr < IO_START) begin
                 frame_buffer[waddr - FRAMEBUFFER_START] <= wdata;
+            end else if (waddr == SCALE_REG) begin
+                scale_reg <= wdata;
+            end else if (waddr == HSCROLL_REG) begin
+                hscroll_reg <= wdata;
+            end else if (waddr == VSCROLL_REG) begin
+                vscroll_reg <= wdata;
             end
         end
     end
